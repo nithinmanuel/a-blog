a37b1d8a64532d2cd797ea6c946ba21c52767ffb
nithinmanuel <nithinmanuel9@gmail.com>
2018-11-29 17:18:38 +0100

    a-blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all 
+  end  	
   def show
       @article = Article.find(params[:id])
    end  
   def new
   end
-  
+
   def create
   	@article = Article.new(article_params)
 
@@ -16,6 +19,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :text)
     end
-  	
 end
 
